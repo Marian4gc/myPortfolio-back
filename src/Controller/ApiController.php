46462eedd7caa730289918f8b0fbd1d40140f070
Marian4gc <mariangarcia1724@gmail.com>
2023-03-13 16:55:52 +0100

    api creada y conectada con front

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Portfolio;
+use App\Form\PortfolioType;
+use App\Repository\PortfolioRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/apiportfolio')]
+class ApiController extends AbstractController
+{
+    #[Route('/list', name: 'app_apiportfolio_index', methods: ['GET'])]
+    public function index(PortfolioRepository $portfolioRepository): Response
+    {
+        $portfolio = $portfolioRepository->findAll();
+
+        $data = [];
+
+        foreach ($portfolio as $p) {
+            $data[] = [
+                'id' => $p->getId(),
+                'name' => $p->getName(),
+                'description' => $p->getDescription(),
+                'image' => $p->getImage(),
+            ];
+            
+        }
+
+        //dump($data);die; Vich-uploader
+        //return $this->json($data);
+        return $this->json($data, $status = 200, $headers = ['Access-Control-Allow-Origin'=>'*']);
+    }
+    }
+
---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Portfolio;
+use App\Form\PortfolioType;
+use App\Repository\PortfolioRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/portfolio')]
+class PortfolioController extends AbstractController
+{
+    #[Route('/', name: 'app_portfolio_index', methods: ['GET'])]
+    public function index(PortfolioRepository $portfolioRepository): Response
+    {
+        return $this->render('portfolio/index.html.twig', [
+            'portfolios' => $portfolioRepository->findAll(),
+        ]);
+    }
+
+    
+    public function create(Request $request)
+{
+    $portfolio = new Portfolio();
+    $form = $this->createForm(PortfolioType::class, $portfolio);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        // Procesar la imagen
+        $imageFile = $form->get('image')->getData();
+        $newFilename = uniqid().'.'.$imageFile->guessExtension();
+
+        // Mover el archivo a la ubicación deseada
+        $imageFile->move(
+            $this->getParameter('images_directory'),
+            $newFilename
+        );
+
+        // Almacenar la ruta de la imagen en la entidad Portfolio
+        $portfolio->setImage($newFilename);
+
+        // Guardar la entidad en la base de datos
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($portfolio);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('portfolio_show', ['id' => $portfolio->getId()]);
+    }
+
+    return $this->render('portfolio/create.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+    #[Route('/new', name: 'app_portfolio_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, PortfolioRepository $portfolioRepository): Response
+    {
+        $portfolio = new Portfolio();
+        $form = $this->createForm(PortfolioType::class, $portfolio);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $portfolioRepository->save($portfolio, true);
+
+            return $this->redirectToRoute('app_portfolio_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('portfolio/new.html.twig', [
+            'portfolio' => $portfolio,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_portfolio_show', methods: ['GET'])]
+    public function show(Portfolio $portfolio): Response
+    {
+        return $this->render('portfolio/show.html.twig', [
+            'portfolio' => $portfolio,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_portfolio_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Portfolio $portfolio, PortfolioRepository $portfolioRepository): Response
+    {
+        $form = $this->createForm(PortfolioType::class, $portfolio);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $portfolioRepository->save($portfolio, true);
+
+            return $this->redirectToRoute('app_portfolio_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('portfolio/edit.html.twig', [
+            'portfolio' => $portfolio,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_portfolio_delete', methods: ['POST'])]
+    public function delete(Request $request, Portfolio $portfolio, PortfolioRepository $portfolioRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$portfolio->getId(), $request->request->get('_token'))) {
+            $portfolioRepository->remove($portfolio, true);
+        }
+
+        return $this->redirectToRoute('app_portfolio_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
